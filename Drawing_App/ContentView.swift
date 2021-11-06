3260b05d35af9154bbd25c1219b57f50a5962759
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-11-06 08:54:26 -0700

    Initial Commit- MVP of project

--- Drawing_App/ContentView.swift
+++ Drawing_App/ContentView.swift
@@ -6,16 +6,64 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        DrawingHome()
     }
 }
+
+struct DrawingHome: View {
+    @State var canvas = PKCanvasView()
+    @State var isDraw = true
+    @State var color: Color = .blue
+    var body: some View{
+        NavigationView{
+            //Drawing View
+            VStack {
+                DrawingView(canvas: $canvas, isDraw: $isDraw, color: $color)
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        HStack {
+                            Button(isDraw ? "| Erase |" : "Draw") {
+                                isDraw.toggle()
+                            }
+                            .padding()
+                            ColorPicker("Color Picker", selection: $color)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DrawingView : UIViewRepresentable {
+    @Binding var canvas: PKCanvasView
+    @Binding var isDraw: Bool
+    @Binding var color: Color
+    
+    let eraser = PKEraserTool(.bitmap)
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        let ink = PKInkingTool(.pencil, color:UIColor(color))
+        canvas.drawingPolicy = .anyInput
+        canvas.tool = isDraw ? ink : eraser
+        return canvas
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        //updating tool whenever mainView updates
+        let ink = PKInkingTool(.pencil, color:UIColor(color))
+        uiView.tool = isDraw ? ink : eraser
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
