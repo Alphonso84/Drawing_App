922bd40143f90209a1978fd23797291a336868b2
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-11-06 08:50:29 -0700

    Initial Commit

---
+++ Drawing_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing_App
+//
+//  Created by Alphonso Sensley II on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing_App/Drawing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_AppApp.swift
+//  Drawing_App
+//
+//  Created by Alphonso Sensley II on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
